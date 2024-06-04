c866d81466cadb1e31e851ac4f2b9c05bc9f794f
priyanshu prajapati <86232180+bypriyan@users.noreply.github.com>
2024-06-04 15:48:58 +0530

    data successful commit

--- app/src/main/java/com/bypriyan/friendshub/MyJavaScriptInterface.kt
+++ app/src/main/java/com/bypriyan/friendshub/MyJavaScriptInterface.kt
@@ -2,6 +2,7 @@ package com.bypriyan.friendshub
 
 import android.util.Log
 import android.webkit.JavascriptInterface
+import android.widget.Toast
 import org.jsoup.Jsoup
 
 class MyJavaScriptInterface {
@@ -11,6 +12,12 @@ class MyJavaScriptInterface {
         val elements = doc.select("[style*=rgb]")
 
         var dataList = mutableListOf<ModelScrapedValues>()
+        var recentData = FloatArray(5)
+        var sixthData  = 0.0f
+        var redColorAppeared = false
+        var conservativePinkColorsList = mutableListOf<ModelScrapedValues>()
+        var blueColorsList = mutableListOf<ModelScrapedValues>()
+        var dataLessThan1p90List = mutableListOf<ModelScrapedValues>()
 
         for (element in elements) {
             val styleAttribute = element.attr("style")
@@ -25,8 +32,30 @@ class MyJavaScriptInterface {
                 }
             }
         }//loop
+
+        var count =0
+            //25
         dataList.takeLast(25).forEach{
-            Log.d("lelo ", " dataList: ${it.value}")
+            if(count<5){
+                recentData[count]=it.value.replace("x","").toFloat()
+            }
+            if(count==5){
+                sixthData=it.value.replace("x","").toFloat()
+            }
+//            Log.d("lelo ", " color ${it.rgb} value ${it.value.replace("x","")}")
+            count++
+        }
+
+        recentData.forEach {
+            Log.d("lelo ", "recent $it")
+        }
+        Log.d("lelo ", "sixth $sixthData")
+
+        if (recentData.all { it < 7.00 } && recentData.any { it >= 1.00 } && sixthData < 8.00) {
+            Log.d("lelo ", "good")
+        }else{
+            Log.d("lelo ", "not good")
+
         }
 
     }
