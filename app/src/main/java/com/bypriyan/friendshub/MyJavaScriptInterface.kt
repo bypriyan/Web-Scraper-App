d495a77f6ae509311f5f5e13913de6cc7c0e1f6e
priyanshu prajapati <86232180+bypriyan@users.noreply.github.com>
2024-06-04 16:45:24 +0530

    2nd step done commit

--- app/src/main/java/com/bypriyan/friendshub/MyJavaScriptInterface.kt
+++ app/src/main/java/com/bypriyan/friendshub/MyJavaScriptInterface.kt
@@ -3,6 +3,7 @@ package com.bypriyan.friendshub
 import android.util.Log
 import android.webkit.JavascriptInterface
 import android.widget.Toast
+import com.bypriyan.bustrackingsystem.utility.Constants
 import org.jsoup.Jsoup
 
 class MyJavaScriptInterface {
@@ -12,7 +13,7 @@ class MyJavaScriptInterface {
         val elements = doc.select("[style*=rgb]")
 
         var dataList = mutableListOf<ModelScrapedValues>()
-        var recentData = FloatArray(5)
+        var recentData = FloatArray(9)
         var sixthData  = 0.0f
         var redColorAppeared = false
         var conservativePinkColorsList = mutableListOf<ModelScrapedValues>()
@@ -36,26 +37,37 @@ class MyJavaScriptInterface {
         var count =0
             //25
         dataList.takeLast(25).forEach{
-            if(count<5){
+            if(count<9){
                 recentData[count]=it.value.replace("x","").toFloat()
             }
-            if(count==5){
+            if(count==9){
                 sixthData=it.value.replace("x","").toFloat()
             }
-//            Log.d("lelo ", " color ${it.rgb} value ${it.value.replace("x","")}")
+
+            if(it.rgb.equals(Constants.KEY_PINK_COLOR)){
+                redColorAppeared=true
+//            Log.d("lelo ", " color ${it.rgb} value ${it.value.replace("x","")}"
+            }
             count++
         }
 
         recentData.forEach {
-            Log.d("lelo ", "recent $it")
+            Log.d("lelo ", "recentData $it")
         }
-        Log.d("lelo ", "sixth $sixthData")
 
-        if (recentData.all { it < 7.00 } && recentData.any { it >= 1.00 } && sixthData < 8.00) {
-            Log.d("lelo ", "good")
-        }else{
-            Log.d("lelo ", "not good")
+        //1st
+        if (recentData.take(5).all { it < 7.00 } && recentData.take(5).any { it >= 1.00 } && sixthData < 8.00) {
+            Log.d("lelo ", "1st good")
+        }
 
+        Log.d("lelo", "showHTML: red color $redColorAppeared")
+        //2nd
+        if (redColorAppeared) {
+            val last6Data = recentData.take(6)
+            if (last6Data.all { it < 8.00 }) {
+                Log.d("lelo", "showHTML: 2nd GOOD")
+                // Continue to number 3
+            }
         }
 
     }
